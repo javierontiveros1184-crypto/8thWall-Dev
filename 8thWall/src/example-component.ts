d97aecc7cd9d67af57d34d788dc9b16833b16ba4
javierontiveros1184-crypto <javier.ontiveros1184@alumnos.udg.mx>
2026-08-11 16:29:25 -0500

    Prueba RRSS

--- 8thWall/src/example-component.ts
+++ 8thWall/src/example-component.ts
@@ -1,8 +1,25 @@
 import * as ecs from '@8thwall/ecs'
 
 ecs.registerComponent({
-  name: 'example-component',
-  add: () => {
-    console.log('Component attached.')
+  name: 'Toggle Animation',
+
+  schema: {
+    target: ecs.eid,
   },
+
+  stateMachine: ({world, eid, schemaAttribute}) => {
+
+    let Stretching = true
+
+    ecs.defineState('default')
+      .initial()
+      .listen(eid, ecs.input.SCREEN_TOUCH_START, () => {
+        const {target} = schemaAttribute.get(eid)
+        Stretching = !Stretching
+
+        ecs.GltfModel.set(world, target, {
+          animationClip: Stretching ? 'Stretching' : 'Floating',
+        })
+      })
+  }
 })
